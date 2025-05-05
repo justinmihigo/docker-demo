4bb050f95629d1940fbc5d5bfb065996023894e2
justinmihigo <mihigojustin28@gmail.com>
2025-05-05 10:33:39 +0200

    feature(update app.py): change a line to see if the sync will happen automatically

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello from Docker From kubernetes!<h1><p>Let me see if the changes have gone through</p>"
+    return "<h1>Hello from Docker From kubernetes!<h1><p>Let me see if the changes have gone through v2 this are new</p>"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
